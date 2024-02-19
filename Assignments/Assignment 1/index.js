f514bf096cc3c27f369701a40dbd5582c103fd81
M Bilal Naeem <119736324+Bilal-Naeem12@users.noreply.github.com>
2024-02-19 16:04:29 +0500

    labTask1

---
+++ Assignments/Assignment 1/index.js
@@ -0,0 +1,37 @@
+let innerProgress = document.querySelectorAll(".inner-progress")
+
+console.log(innerProgress)
+
+
+innerProgress.forEach((e)=>
+{
+incremental(e)
+}
+);
+function updateProgress(progress,progressBar) {
+    progressBar.style.width = progress + '%';
+  
+  }
+
+function incremental(e){
+    
+
+const cssPropertyValue =  e.attributes[1].textContent;
+const numericValue = parseFloat(cssPropertyValue.match(/\d+/)[0]);
+console.log(numericValue)
+
+let progress = 0;
+    let interval = setInterval(function() {
+      progress += 1;
+      updateProgress(progress,e);
+
+      if (progress >= numericValue) {
+        clearInterval(interval);
+      }
+    }, 10);
+
+}
+
+function simulateProgress() {
+    
+  }
