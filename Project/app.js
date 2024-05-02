172b0d7d4ede5784aa3325a84ea049dc49f8bacb
M Bilal Naeem <119736324+Bilal-Naeem12@users.noreply.github.com>
2024-05-02 18:28:58 +0500

    converted project to ejs

--- Project/app.js
+++ Project/app.js
@@ -1,25 +1,21 @@
 var express = require('express');
-var path = require('path');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var productRouter = require('./routes/product');
 
 var app = express();
-
+app.use(express.static('public'));
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/home', indexRouter);
-app.use('/users', usersRouter);
 
 
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use('/', indexRouter);
+app.use('/products', productRouter);
+
+
+
 
 
 app.listen(3000)
