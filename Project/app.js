f951d76afe5c1055171e5a2ce83ca3b7aa73f9d4
M Bilal Naeem <119736324+Bilal-Naeem12@users.noreply.github.com>
2024-05-08 18:27:16 +0500

    checkout cart

--- Project/app.js
+++ Project/app.js
@@ -2,7 +2,11 @@ var express = require('express');
 var mongoose = require('mongoose')
 var indexRouter = require('./routes/index');
 var productRouter = require('./routes/product');
+var cartRouter = require('./routes/cart');
+
+
 const Watches = require("./modal/Watch")
+
 mongoose.connect("mongodb://127.0.0.1:27017/SemsterProject").then(()=>{
   console.log("connected")
 })
@@ -17,6 +21,8 @@ app.use(express.json());
 
 app.use('/', indexRouter);
 app.use('/products', productRouter);
+app.use('/cart', cartRouter);
+
 
 app.get("/db",async (req,res)=>{
   const ans = await Watches.find();
